e5a760d523f12d3f3bb89af168707c700348041a
Paulina Guzy <paulinaguzy@gmail.com>
2015-02-27 09:48:25 +0100

    Instalacja FOSUser bundle i uploadimage bundle

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,37 @@
+<?php
+// src/Acme/UserBundle/Entity/User.php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="address", type="string", length=500, nullable=false)
+     */
+    protected $address;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+
+
+
+}
